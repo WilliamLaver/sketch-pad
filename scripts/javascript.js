070fa2a7797fccf1f69bd86e46241cd506a3f4d4
William Laver <george.laver@mail.mcgill.ca>
2016-09-30 18:45:10 -0600

    Made a fairly functional sketchpad, further revisions possible but not required.

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -1,13 +1,64 @@
 $(document).ready(function(){
 
-	for (var i = 0; i < 16*16; i++) {
-		if((i % 16) == 0){
-			$('.container').append('<div class="square" style="clear:both"></div>');
+	var action = "draw";
+	var draw_focused = false;
+	var numPixels = 92;
+
+	for (var i = 0; i < numPixels*numPixels; i++) {
+		if((i % numPixels) == 0){
+			$('.container').append('<div class="square" style="clear:left"></div>');
 
 		}else{
 			$('.container').append('<div class="square"></div>');
 		}
 	}
+
+	var pixelHeight = 540/numPixels;
+	$('.square').css('height',pixelHeight);
+	$('.square').css('width', pixelHeight);
+
+	$('.square').click(function(){
+		if (draw_focused) {
+			draw_focused = false;
+		}else{
+			draw_focused = true;
+		}
+	});
 	
+	$('.square').hover(
+
+		function(){
+			if (action == "draw" && draw_focused) {
+				$(this).addClass('yellow');
+			}else if(action == "erase"){
+				$(this).removeClass('yellow');
+			}
+			
+		}
+
+	);
+
+	//handle button click event
+	$('button').click(function(){
+		
+		if( $(this).attr('name') == "clear" ){
+			clearSketch();
+		}else{
+			$('button').removeClass('clicked');
+			$(this).addClass('clicked');
+			action = $(this).attr('name');
+		}
+
+		
+	});
 
 });
+
+
+function clearSketch(){
+	
+	$('.container div').removeClass('yellow');
+}
+
+
+
